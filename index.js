053bbb55f176201f4b0cdf96cdd7c826b7b72fa5
tushort <samquioh@gmail.com>
2023-07-25 07:13:25 -0700

    add new list item

---
+++ index.js
@@ -0,0 +1,6 @@
+let id = document.getElementById('head');
+id.style.color = 'green'
+
+let newListItem = document.createElement('li')
+newListItem.textContent - 'Jalapenos'
+document.querySelector('ul').appendChild(newListItem)
