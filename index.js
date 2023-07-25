4c84242367adc63fde62788b3b7fb2f5e7cc3528
tushort <samquioh@gmail.com>
2023-07-25 07:27:17 -0700

    add new List item

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let id = document.getElementById('head');
 id.style.color = 'green'
-
+//create a new list item and store it in a variable.
 let newListItem = document.createElement('li')
+
+// Now let's update the text content of that list item.
 newListItem.textContent - 'Jalapenos'
+
+//add that list item as a child of the ul.
 document.querySelector('ul').appendChild(newListItem)
+
+let myDiv = document.getElementById('myDiv')
+
+// create a checkbox element
+let checkbox = document.createElement("input")
