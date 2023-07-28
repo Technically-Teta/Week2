ccdb1f79d2d40bf9114fdd04bfe3710cabfd249e
tushort <samquioh@gmail.com>
2023-07-27 22:25:01 -0700

    add updated index.js from repl

--- index.js
+++ index.js
@@ -1,95 +1,85 @@
+// /////////////////LIST CREATION PROCESS 
+// let ul = document.querySelector('ul');
+
+// ul.addEventListener('click', function handleClick() {
+//   // This code will be executed when the user clicks on the ul element.
+//   let newListItem = document.createElement('li');
+//   newListItem.textContent = 'Jalapenos';
+//   ul.appendChild(newListItem);
+// });
 
 
+// let checkbox = document.createElement('input');
+// checkbox.type = 'checkbox';
+// checkbox.name = 'name';
+// checkbox.value = 'value';
+// checkbox.id = 'checkboxId';
+// checkbox.className = 'visible';
 
-/////////////////LIST CREATION PROCESS 
-//create a new list item and store it in a variable.
-let newListItem = document.createElement('li')
-
-// Now let's update the text content of that list item.
-newListItem.textContent = 'Jalapenos'
-
-//add that list item as a child of the ul.
-document.querySelector('ul').appendChild(newListItem)
+// ul.appendChild(checkbox);
 
 
-////////////CHECKBOX CREATION PROCESS
-const myDiv = document.getElementById("myDiv")
+// //make the label for the checkbox
 
-// create a checkbox element
-let checkbox = document.createElement("input")
+// let label = document.createElement("label")
 
-//Assign attributues to the created checkbox
+// //assign attributues for the label
+// label.htmlFor = "checkboxId";
 
-checkbox.type = "checkbox"
-checkbox.name = "name"
-checkbox.value = " value"
-checkbox.id = "checkboxId"
-checkbox.className = "visible"
+// //append the created text to the created label
+// label.appendChild(document.createTextNode("This creates the label for the checkbox"))
 
-//make the label for the checkbox
+// // append checkbox and label to div
+// myDiv.appendChild(checkbox)
+// myDiv.appendChild(label)
 
-let label = document.createElement("label")
-
-//assign attributues for the label
-label.htmlFor = "checkboxId";
-
-//append the created text to the created label
-label.appendChild( document.createTextNode("This creates the label for the checkbox"))
-
-// append checkbox and label to div
-myDiv.appendChild(checkbox)
-myDiv.appendChild(label)
-
-//ADDITION 1
+//ADDITION 1: 
 //change the document's text color on click:
 
 //create the button and assign the id
 let btn = document.getElementById('btn')
 
 // add event listener
-btn.addEventListener('click',function onclick(event) {
-    //let this apply to the whole body
-  document.body.style.color = 'darkgreen';
-  event.target.style.color = 'salmon'
+btn.addEventListener('click', function () {
+  //let this apply to the first paragraph 
+  let  firstP = document.getElementById("mydescription");
+  console.log(firstP);
+  firstP.style.color = 'red';
+  firstP.style.backgroundColor = 'yellow';
+ 
 })
 
-// ADDITION 2
-//change an element's text color on mouseover:
+// // ADDITION 2:
+// //change an element's text color on mouseover:
 const box = document.getElementById('box');
 
-// 👇️ Change text color on mouseover
+// //  Change text color on mouseover
 box.addEventListener('mouseover', function handleMouseOver() {
-  box.style.color = 'red';
+   box.style.color = 'red';
 });
 
-// 👇️ Change text color back on mouseout
+// // Change text color back on mouseout
 box.addEventListener('mouseout', function handleMouseOut() {
   box.style.color = 'black';
 });
 
-//ADDITION 3 
+//ADDITION 3: 
 //add the date and time to the site up on entry
 
 // Function to get the current date and time
 function getCurrentDateTime() {
-    const now = new Date();
-    const date = now.toDateString(); // Get the date in human-readable format
-    const time = now.toLocaleTimeString(); // Get the time in human-readable format
+  let now = new Date();
+  let date = now.toDateString(); // Get the date in human-readable format
+  let time = now.toLocaleTimeString(); // Get the time in human-readable format
+  let pTag = document.getElementById("dateTimeDisplay");
+  pTag.innerHTML = `current date: ${date} time: ${time}`
+    
 
-    return { date, time };
 }
-
-// Function to update the HTML element with the current date and time
-function updateDateTimeDisplay() {
-    const dateTime = getCurrentDateTime();
-    const dateTimeDisplayElement = document.getElementById('dateTimeDisplay');
-
-    // Update the HTML element content
-    dateTimeDisplayElement.textContent = `Current date: ${dateTime.date}, Current time: ${dateTime.time}`;
-}
-
 // Call the function when the page loads
-window.addEventListener('load', updateDateTimeDisplay);
+getCurrentDateTime();
 
 
 
+
+ 
